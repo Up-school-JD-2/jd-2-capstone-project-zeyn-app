ace5f95ad3403c1e643108dbe9d04cddb94b9c25
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-08-06 02:40:11 +0300

    request added to create flight on Airline Company

---
+++ src/main/java/io/upschool/dto/flightDto/CompanyFlightRequest.java
@@ -0,0 +1,17 @@
+package io.upschool.dto.flightDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CompanyFlightRequest {
+    private LocalDateTime departureDateTime; // 2023-08-05T10:15:30
+    private Long routeId;
+}
