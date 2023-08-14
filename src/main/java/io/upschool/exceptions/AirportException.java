e3d2aa111f7e88cd92e949ecb84261a02699b97b
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-08-14 22:30:30 +0300

    made exception classes final

--- src/main/java/io/upschool/exceptions/AirportException.java
+++ src/main/java/io/upschool/exceptions/AirportException.java
@@ -1,6 +1,6 @@
 package io.upschool.exceptions;
 
-public class AirportException extends RuntimeException {
+public final class AirportException extends RuntimeException {
     public final static String DATA_NOT_FOUND = "Airport not found";
     public final static String AIRPORT_EXIST = "Airport exist";
 
