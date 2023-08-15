4953624cd8c37247399d09933a710a7f089dc8b0
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-08-15 13:52:28 +0300

    added route searching

--- src/main/java/io/upschool/repository/RouteRepository.java
+++ src/main/java/io/upschool/repository/RouteRepository.java
@@ -4,7 +4,10 @@ import io.upschool.entity.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RouteRepository extends JpaRepository<Route, Long> {
     boolean existsAllByArrivalAirport_NameAndDepartureAirport_Name(String arrivalAirportName, String departureAirportName);
+    List<Route> findAllByDepartureAirport_LocationAndArrivalAirport_Location(String departureAirportLocation, String arrivalAirportLocation);
 }
